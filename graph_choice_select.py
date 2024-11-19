271a04f67270afa0a7c3e53a9bac4812bde79b11
Carlo Fialdini <cfialdini.ieu2023@student.ie.edu>
2024-11-19 11:37:11 +0100

    Here I am writting the pseudo code for the chart selection option fot the user.

---
+++ graph_choice_select.py
@@ -0,0 +1,11 @@
+
+
+
+#The user will have selected the number of variables he wants and assigned it as independent or dependent
+#The user will select the graph of interest based on the list of graphs available
+
+#if the user selects a histogram, then he must have selected only one variable
+    #Plot the histogram of the variable selected by the user
+#if the user selects bar chart, he needs 2 variables
+    #plot the bar chart based on the two variables selected by the user
+#if the user selects a Box plot then be must have selected a maxim
