1969a9911080120a7c786c8e8a45cc18f8f9a21b
MeropiLefka <mlefkaditi05@gmail.com>
2024-12-17 21:15:07 +0100

    Update Graphsuggest.py

--- Graphsuggest.py
+++ Graphsuggest.py
@@ -68,55 +68,44 @@ def Piechart(column, filename):
     plt.figure(dpi=96) 
     counts = data[column].value_counts()
     num_colors = len(counts)
-    dark_green_shades = plt.cm.Greens(np.linspace(0.5, 1, len(counts)))  # Darker shades of green
+    dark_green_shades = plt.cm.Greens(np.linspace(0.5, 1, len(counts)))  
     explode = [0.2 if count == max(counts.values) else 0 for count in counts.values]
     plt.pie(counts.values, labels=counts.index, autopct='%1.1f%%', startangle=90,
             explode=explode, colors=dark_green_shades, counterclock=False, shadow=True)
    
-    #plt.show(block=True)
+    
     plt.savefig(filename,dpi=96) 
     
 
 def Scatterplot(x, y, filename):
     plt.figure(dpi=96) 
-    plt.scatter(data[x], data[y], color='#006400', alpha=0.7, edgecolor='black')  # Dark Green
-    plt.xlabel(f'{x}', color='black')  # X-axis label color to black
-    plt.ylabel(f'{y}', color='black')  # Y-axis label color to black
+    plt.scatter(data[x], data[y], color='#006400', alpha=0.7, edgecolor='black')  
+    plt.xlabel(f'{x}', color='black')  
+    plt.ylabel(f'{y}', color='black')  
     
-    #plt.show(block=True)
+   
     plt.savefig(filename,dpi=96) 
    
 
 
 def Linechart(x, y, filename):
     plt.figure(dpi=96) 
-    plt.plot(data[x], data[y], color='#006400', alpha=0.7)  # Dark Green
-    plt.xlabel(f'{x}', color='black')  # X-axis label color to black
-    plt.ylabel(f'{y}', color='black')  # Y-axis label color to black
+    plt.plot(data[x], data[y], color='#006400', alpha=0.7)  
+    plt.xlabel(f'{x}', color='black') 
+    plt.ylabel(f'{y}', color='black') 
     
-    #plt.show(block=True)
+
     plt.savefig(filename,dpi=96) 
    
 
 def StackedBarchart(x, y, filename):
     plt.figure(dpi=96) 
-    cross_tab = pd.crosstab(data[x], data[y])
-
-    # Create the stacked bar chart with custom colormap
-    colors = plt.cm.Greens(np.linspace(0.3, 1, len(cross_tab.columns)))  # Adjust the color range (0.3 to 1)
-    ax = cross_tab.plot(kind='bar', stacked=True, color=colors)  # Use custom colors
-
-    # Set the x and y axis labels
-    plt.xlabel(f'{x}', color='black')  # X-axis label color to black
-    plt.ylabel(f'{y}', color='black')  # Y-axis label color to black
-
-    # Adjust the position of the legend to be outside the plot area
+    cross_tab = pd.crosstab(data[x], data[y])   
+    colors = plt.cm.Greens(np.linspace(0.3, 1, len(cross_tab.columns)))  
+    ax = cross_tab.plot(kind='bar', stacked=True, color=colors)      
+    plt.xlabel(f'{x}', color='black')  
+    plt.ylabel(f'{y}', color='black') 
     plt.legend(title=y, bbox_to_anchor=(1.05, 1), loc='upper left', frameon=False)
-
-    # Save the figure
-    #plt.tight_layout()  # To ensure the legend fits without overlapping
-   
-    #plt.show(block=True)
     plt.savefig(filename,dpi=96) 
   
 
@@ -126,15 +115,13 @@ def StackedBarchart(x, y, filename):
 def Heatmap(x, y, filename):
     pivot_table = data.pivot_table(index=x, columns=y, aggfunc='size', fill_value=0)
     plt.figure(dpi=96) 
-    cmap = plt.cm.Greens(np.linspace(0.3, 1, 256))  # Darker greens range
+    cmap = plt.cm.Greens(np.linspace(0.3, 1, 256))  
     plt.imshow(pivot_table, cmap=ListedColormap(cmap), interpolation='nearest')
-    plt.colorbar(label='Counts')  # Color bar label color to black
-    plt.xlabel(f'{x}', color='black')  # X-axis label color to black
-    plt.ylabel(f'{y}', color='black')  # Y-axis label color to black
-    plt.title(f'Heatmap of {x} vs {y}', color='black')  # Set title color to black
+    plt.colorbar(label='Counts')  
+    plt.xlabel(f'{x}', color='black')  
+    plt.ylabel(f'{y}', color='black')  
+    plt.title(f'Heatmap of {x} vs {y}', color='black')  
     plt.tight_layout()
-    
-    #plt.show(block=True)
     plt.savefig(filename,dpi=96) 
    
 
@@ -148,20 +135,16 @@ def HeatmapWithTwoNumericals(categorical, numerical_x, numerical_color, data, ag
 
 
     plt.figure(dpi=96) 
-    cmap = plt.cm.Greens(np.linspace(0.3, 1, 256))  # Darker range for better visibility in green
-    sns.heatmap(heatmap_data,
-                cmap=ListedColormap(cmap),  # Apply custom green colormap
-                annot=False,  # Do not annotate cells with values
-                fmt='.2f',  # Format of annotations if annot is True
-                cbar_kws={'label': numerical_color},  # Color bar label color to black
-                linewidths=0.5)  # Optional: make gridlines thinner
+    cmap = plt.cm.Greens(np.linspace(0.3, 1, 256))  
+                cmap=ListedColormap(cmap), 
+                annot=False,  
+                fmt='.2f', 
+                cbar_kws={'label': numerical_color}, 
+                linewidths=0.5)  
 
-    plt.xlabel(f'{numerical_x}', color='black')  # X-axis label color to black
-    plt.ylabel(f'{categorical}', color='black')  # Y-axis label color to black
-
-    plt.tight_layout()  # To ensure the layout is properly adjusted
-      
-    #plt.show(block=True)
+    plt.xlabel(f'{numerical_x}', color='black')  
+    plt.ylabel(f'{categorical}', color='black')  
+    plt.tight_layout()  
     plt.savefig(filename,dpi=96) 
    
 
@@ -169,11 +152,9 @@ def ThreeDScatterplot(numvar1, numvar2, numvar3, filename, data):
     plane = plt.figure(dpi=96) 
     axis = plane.add_subplot(projection='3d')
     axis.scatter(data[numvar1], data[numvar2], data[numvar3],c="green")
-    axis.set_xlabel(f'{numvar1}', color="black")  # Set axis label color to black
-    axis.set_ylabel(f'{numvar2}', color="black")  # Set axis label color to black
-    axis.set_zlabel(f'{numvar3}', color="black")  # Set axis label color to black
-       
-    #plane.show(block=True)
+    axis.set_xlabel(f'{numvar1}', color="black")  
+    axis.set_ylabel(f'{numvar2}', color="black")  
+    axis.set_zlabel(f'{numvar3}', color="black")  
     plt.savefig(filename,dpi=96) 
 
 
@@ -189,11 +170,11 @@ def VariableSelection():
     title = create_label(win, "Select Variables (Up to 3)", Point(400, 50), 20, colorcream, "bold")
   
 
-    up_arrow = Polygon(Point(740, 170), Point(710, 200), Point(770, 200))  # Smaller triangle pointing up
+    up_arrow = Polygon(Point(740, 170), Point(710, 200), Point(770, 200))  
     up_arrow.setFill("white")
     up_arrow.draw(win)
 
-    down_arrow = Polygon(Point(740, 440), Point(710, 410), Point(770, 410))  # Smaller triangle pointing down
+    down_arrow = Polygon(Point(740, 440), Point(710, 410), Point(770, 410))  
     down_arrow.setFill("white")
     down_arrow.draw(win)
     
@@ -207,12 +188,12 @@ def VariableSelection():
     texts = []
     selected = []
     y_start = 100
-    visible_count = 6  # Number of items visible at a time
-    scroll_offset = 0  # Track the scroll position
+    visible_count = 6  
+    scroll_offset = 0  
 
-    x_start = 120  # Position them 100px from the left side of the window
+    x_start = 120  
 
-    # Create a list of rectangles and placeholder texts for all variables
+
     for i in range(visible_count):
         rect, text =create_button(win,Point(x_start, y_start + i * 70), Point(x_start + 550, y_start + i * 70 + 50), "", "lightgrey","black",vout="lightgrey",size=12)
        
@@ -225,7 +206,7 @@ def VariableSelection():
         for text in texts:
             text.undraw()
 
-        # Determine which variables to display
+        
         start = scroll_offset * visible_count
         end = start + visible_count
 
@@ -234,19 +215,19 @@ def VariableSelection():
             rect = rectangles[index]
             text = texts[index]
 
-            # Update rectangle color based on selection
+            
             rect.setFill("lightgreen" if data.columns[i] in selected else "lightgrey")
 
-            # Update text to match variable name
+           
             text.setText(data.columns[i])
 
-            # Draw the updated rectangle and text
+            
             rect.draw(win)
             text.draw(win)
 
     draw_visible()
 
-    # Submit button positioned at the bottom center
+    
     submit_btn, submit_text =create_button(win,Point(200, 520), Point(300, 570), "Submit", "lightgreen","darkgreen",vout="lightgreen",size=12)
     back_button, vim = create_button(win, Point(350, 520), Point(550, 570), "Go to Dashboard", "lightgreen", "Darkgreen",size=12)   
   
@@ -254,30 +235,28 @@ def VariableSelection():
     while True:
         click = win.getMouse()
 
-        if is_click_in_rectangle(click, submit_btn): # Submit button clicked
+        if is_click_in_rectangle(click, submit_btn): 
             break
         elif is_click_in_rectangle(click, back_button):
             win.close()           
             create_dashboard()
 
-        # Check for clicks on the up arrow (right side)
-        elif 710 < click.x < 770 and 160 < click.y < 200:  # Up arrow clicked
+        elif 710 < click.x < 770 and 160 < click.y < 200: 
 
             if scroll_offset > 0:
                 scroll_offset -= 1
                 draw_visible()
 
-        # Check for clicks on the down arrow (right side)
-        elif  710 < click.x < 770 and 410 < click.y < 440:  # Down arrow clicked
+        elif  710 < click.x < 770 and 410 < click.y < 440: 
 
             if (scroll_offset + 1) * visible_count < len(data.columns):
                 scroll_offset += 1
                 draw_visible()
                 
-        if 50 < click.x < 100 and 55 < click.y < 58:  # Back arrow region  # If the back button is clicked            
+        if 50 < click.x < 100 and 55 < click.y < 58:       
             main()
 
-        # Check for clicks on visible rectangles
+       
         start = scroll_offset * visible_count
         end = start + visible_count
 
@@ -369,13 +348,13 @@ def GraphOptions(selected):
                 break
             
 
-        if 50 < click.x < 100 and 55 < click.y < 58:  # Back arrow region  # If the back button is clicked            
+        if 50 < click.x < 100 and 55 < click.y < 58:       
             main()
 
         if selected_option:
             break
     win.close()
-    return selected_option, selected  # Return the selected graph and variables
+    return selected_option, selected 
 
 
 
@@ -394,19 +373,19 @@ def DisplayGraph(filename, title_text):
     graph_image = Image(Point(400, 300), filename)
     graph_image.draw(win)
 
-    back_arrow = Line(Point(50, 50), Point(100, 50))  # Starting at (50, 50) to (100, 50)
-    back_arrow.setArrow('first')  # Arrow facing left
+    back_arrow = Line(Point(50, 50), Point(100, 50))  
+    back_arrow.setArrow('first')  
     back_arrow.setWidth(3)
     back_arrow.setFill("white")
     back_arrow.draw(win)
 
     while True:
         click = win.getMouse()
-        if 700 < click.x < 800 and 550 < click.y < 600:  # Close button region
+        if 700 < click.x < 800 and 550 < click.y < 600:  
             break
-        if 50 < click.x < 100 and 40 < click.y < 60:  # Back arrow region
+        if 50 < click.x < 100 and 40 < click.y < 60:  
             main()
-            return True  # Indicating that the user wants to go back
+            return True  
     win.close()
     return False
 
@@ -416,16 +395,16 @@ def main():
         selected_variables = VariableSelection()
 
         if selected_variables:
-            while True:  # Add an inner loop for navigation between variable and graph selection
+            while True:  
                 selected_graph, selected_variables = GraphOptions(selected_variables)
 
-                if selected_graph is None:  # Back arrow clicked
-                    break  # Go back to the variable selection screen
+                if selected_graph is None:  
+                    break 
 
-                # Generate and display the selected graph
+               
                 plt.rcParams['figure.dpi'] = 96
                 plt.rcParams['savefig.dpi'] = 96      
-                plt.close()  # Close   
+                plt.close()  
                 if selected_graph == "Histogram":
                     Histogram(selected_variables[0], "graph.png")
                     title_text = f"Histogram of {selected_variables[0]}"
@@ -455,11 +434,11 @@ def main():
                     title_text = f"3D Scatter Plot for {selected_variables[0]} vs {selected_variables[1]} vs {selected_variables[2]} "
                 elif selected_graph == "Heatmap With Two Numericals":
                     HeatmapWithTwoNumericals(
-                        selected_variables[2],  # Categorical variable for Y-axis
-                        selected_variables[0],  # Numerical variable for X-axis
-                        selected_variables[1],  # Numerical variable for color
+                        selected_variables[2],  
+                        selected_variables[0],  
+                        selected_variables[1],  
                         data=data,
-                        filename="graph.png"# Data to plot
+                        filename="graph.png"
                     )
                     title_text = f"Heatmap: {selected_variables[2]} vs {selected_variables[0]} vs {selected_variables[1]}"
 
@@ -467,7 +446,7 @@ def main():
                     print("Graph type not implemented!")
 
                 if DisplayGraph("graph.png", title_text):
-                    continue  # User clicked back in the graph viewer, return to graph options
+                    continue  
                 else:
                     break
 
